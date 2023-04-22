1efd7e27e682a764aa1811f171198ff1ae6352bb
Russel Benito <59615148+9russelb@users.noreply.github.com>
2023-04-21 18:25:38 -0600

    Started nav implementation

---
+++ src/components/nav.js
@@ -0,0 +1,9 @@
+import styles from '@/styles/nav.module.scss'
+
+export default function Nav() {
+    return (
+        <div>
+            <p>hi</p>
+        </div>
+    )
+}
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,9 @@
+import Nav from '@/components/nav'
+
 export default function About() {
     return (
         <>
+            <Nav />
             <h1>About</h1>
         </>
     )
