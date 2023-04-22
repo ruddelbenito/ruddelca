ed5c733fb72a421c89010821ae7d4d0a7ff1f2b9
Russel Benito <59615148+9russelb@users.noreply.github.com>
2023-04-21 18:49:31 -0600

    Implemented nav to required pages and styled

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,9 @@
+import Nav from '@/components/nav'
+
 export default function Contact() {
     return (
         <>
+            <Nav />
             <h1>Contact</h1>
         </>
     )
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,9 @@
+import Nav from '@/components/nav'
+
 export default function Projects() {
     return (
         <>
+            <Nav />
             <h1>Projects</h1>
         </>
     )
