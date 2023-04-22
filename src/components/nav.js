01db0f7af5f00d9f58f113a8a8ddbfc445883155
Russel Benito <59615148+9russelb@users.noreply.github.com>
2023-04-21 18:34:06 -0600

    Added links to navbar component

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,9 +1,17 @@
 import styles from '@/styles/nav.module.scss'
+import Link from 'next/link'
 
 export default function Nav() {
     return (
-        <div>
-            <p>hi</p>
+        <div className={styles.nav}>
+            <div className={styles.homelink}>
+                <Link href='/'>ruddel</Link>
+            </div>
+            <div className={styles.pagelinks}>
+                <Link href='/about'>About</Link>
+                <Link href='/projects'>Projects</Link>
+                <Link href='/contact'>Contact</Link>
+            </div>
         </div>
     )
 }
