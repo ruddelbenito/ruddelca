c91c9d2de481a8f33afd0e7974a1387f62028ff2
Russel Benito <59615148+9russelb@users.noreply.github.com>
2023-04-23 14:00:09 -0600

    Temporarily removed analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,12 @@
 import '@/styles/globals.scss'
 // Analytics imports
-import { Analytics } from '@vercel/analytics/react'
+// import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
+      {/* <Analytics /> */}
     </>
   )
 }
