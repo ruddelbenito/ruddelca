23e6c69bd7384105bbe6995bf1af818aac61768e
Russel Benito <59615148+9russelb@users.noreply.github.com>
2023-04-23 13:59:15 -0600

    Added introduction

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,24 @@
 import Nav from '@/components/nav'
+import moment from 'moment'
 
 export default function About() {
+    const currentAge = moment().diff(moment('1999-09-09'), 'years', false);
+
     return (
         <>
             <Nav />
             <h1>About</h1>
+            <div>
+                <p>Hi, I'm Russel. I'm a {currentAge}-year-old software developer from Edmonton, Alberta, Canada. </p>
+                <p>
+                    I graduated from the Northern Alberta Institute of Technology (NAIT)'s Digital Media and IT - Computer Software Development program in September 2022.
+                    During my time there, I learned a multitude of languages, skills, and a foundation of knowledge surrounding the software development life cycle
+                    — from programming languages such as C# and JavaScript to project management, communications, and agile/scrum methodologies.
+                </p>
+                {/* <p>
+                    Since starting my software journey, 
+                </p> */}
+            </div>
         </>
     )
 }
