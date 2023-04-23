2a890f9d759e100c7edca4cd0794460b3e991105
Russel Benito <59615148+9russelb@users.noreply.github.com>
2023-04-23 14:03:05 -0600

    Added individual project sections

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,6 +5,21 @@ export default function Projects() {
         <>
             <Nav />
             <h1>Projects</h1>
+
+            {/* PSA */}
+            <div>
+                <h2>Philippine Sports Association (PSA)</h2>
+            </div>
+
+            {/* Surfselect */}
+            <div>
+                <h2>Surfselect</h2>
+            </div>
+
+            {/* Personal Portfolio */}
+            <div>
+                <h2>Personal Portfolio</h2>
+            </div>
         </>
     )
 }
