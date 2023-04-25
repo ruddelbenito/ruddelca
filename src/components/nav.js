b9caa1f9c34c3ea5322e3fd902cbfebbbe6cd69e
Russel Benito <59615148+9russelb@users.noreply.github.com>
2023-04-25 00:00:11 -0600

    Restarted navbar styling

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,18 +1,24 @@
 import styles from '@/styles/nav.module.scss'
 import Link from 'next/link'
 import Icon from '../../public/assets/svgs/menu.svg'
+import { useState } from 'react'
 
 export default function Nav() {
+    function showNav() {
+        let navLinks = document.getElementById('nav-links');
+        navLinks.classList.toggle('active');
+    }
+
     return (
         <div className={styles.nav}>
             <div className={styles.homelink}>
                 <Link href='/'>ruddel</Link>
             </div>
-            <Icon className={styles.icon} />
-            <nav className={styles.pagelinks}>
-                <Link href='/about'>About</Link>
-                <Link href='/projects'>Projects</Link>
-                <Link href='/contact'>Contact</Link>
+            <Icon className={styles.icon} onClick={(e) => showNav()} />
+            <nav className={styles.pageLinkGroup} id='nav-links'>
+                <Link className={styles.pageLink} href='/about'>About</Link>
+                <Link className={styles.pageLink} href='/projects'>Projects</Link>
+                <Link className={styles.pageLink} href='/contact'>Contact</Link>
             </nav>
         </div>
     )
