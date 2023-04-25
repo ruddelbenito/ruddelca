fc323ff6828312338cbdb2b0681ee7768ee890dc
Russel Benito <59615148+9russelb@users.noreply.github.com>
2023-04-25 01:02:32 -0600

    Finished making navbar responsive

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,11 +1,15 @@
 import styles from '@/styles/nav.module.scss'
 import Link from 'next/link'
 import Icon from '../../public/assets/svgs/menu.svg'
+import { useState } from 'react'
 
 export default function Nav() {
+    const [active, setActive] = useState(false);
+
     function showNav() {
-        let navLinks = document.getElementById('nav-links');
-        navLinks.classList.toggle('active');
+        // let navLinks = document.getElementById('nav-links');
+        // navLinks.classList.toggle({ styles.active });
+        setActive(!active);
     }
 
     return (
@@ -14,7 +18,7 @@ export default function Nav() {
                 <Link href='/'>ruddel</Link>
             </div>
             <Icon className={styles.icon} onClick={(e) => showNav()} />
-            <nav className={styles.pageLinkGroup} id='nav-links'>
+            <nav className={`${styles.pageLinkGroup} ${active ? styles.active : ''}`} id='nav-links'>
                 <Link className={styles.pageLink} href='/about'>About</Link>
                 <Link className={styles.pageLink} href='/projects'>Projects</Link>
                 <Link className={styles.pageLink} href='/contact'>Contact</Link>
