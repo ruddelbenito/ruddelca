658a9af6d98a592d19242258eec14a614e6c4c5f
Russel Benito <59615148+9russelb@users.noreply.github.com>
2023-04-26 10:23:13 -0600

    Started contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,6 +2,22 @@ export default function Contact() {
     return (
         <>
             <h1>Contact</h1>
+            <form>
+                <label>Name</label>
+                <input></input>
+
+                <label>Email</label>
+                <input></input>
+
+                <label>Message</label>
+                <textarea />
+
+                <button>Send</button>
+            </form>
+
+            <div>
+                Alternatively, you can email me at ruddel.benito@gmail.com
+            </div>
         </>
     )
 }
