8f8be4cdeae51527eb8d960f60cc4e1e83dac67a
Russel Benito <59615148+9russelb@users.noreply.github.com>
2023-05-01 10:30:09 -0600

    Started About page styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,15 +12,14 @@ export default function About() {
                 <p>Hi, I'm Russel. I'm a {currentAge}-year-old software developer from Edmonton, Alberta, Canada. </p>
             </div>
 
-            <div>
+            <div className={styles.schoolBackground}>
                 <p>
                     I graduated from the Northern Alberta Institute of Technology (NAIT)'s Digital Media and IT - Computer Software Development program in September 2022.
+                </p>
+                <p>
                     During my time there, I learned a multitude of languages, skills, and a foundation of knowledge surrounding the software development life cycle
                     — from programming languages such as C#, SQL, and JavaScript to project management, communications, and agile/scrum methodologies.
                 </p>
-                {/* <p>
-                    Since starting my software journey, 
-                </p> */}
             </div>
         </>
     )
