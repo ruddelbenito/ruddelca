dd87373dcf67a30fbf0939ff879f71410b7bba30
Russel Benito <59615148+9russelb@users.noreply.github.com>
2023-05-09 11:39:15 -0600

    Implemented message sending

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,8 @@
 import { useForm } from 'react-hook-form'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { Alert, AlertTitle } from '@mui/material'
 import styles from '@/styles/contact.module.scss'
+import emailjs from '@emailjs/browser'
 
 export default function Contact() {
     const { register, handleSubmit, errors, reset } = useForm();
@@ -9,10 +10,22 @@ export default function Contact() {
     const [alertSeverity, setAlertSeverity] = useState('success');
     const [alertTitle, setAlertTitle] = useState('');
     const [alertDescription, setAlertDescription] = useState('');
+    const form = useRef();
 
     const onSubmit = (data) => {
         // console.log(data.email)
-        console.log('save attempt')
+
+        emailjs.sendForm(process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID, process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID, form.current, process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY)
+            .then((result) => {
+
+            }, (error) => {
+                setAlertSeverity('warning')
+                setAlertTitle('Could not send message, please try again')
+                setAlertDescription('')
+                setShowAlert(true)
+            }
+            )
+
         setAlertSeverity('success')
         setAlertTitle('Message sent successfully')
         setAlertDescription('Thank you for your message. If it warrants a response, I will reply via email!')
@@ -21,7 +34,7 @@ export default function Contact() {
     }
 
     const onError = () => {
-
+        setShowAlert(false)
     }
 
     const onClear = () => {
@@ -42,15 +55,19 @@ export default function Contact() {
                 <></>
             }
 
-            <form className={styles.contactForm} onSubmit={handleSubmit(onSubmit, onError)}>
+            <form
+                className={styles.contactForm}
+                onSubmit={handleSubmit(onSubmit, onError)}
+                ref={form}
+            >
                 <label className={styles.label}>Name</label>
-                <input className={styles.input} {...register('name', { required: true })}></input>
+                <input className={styles.input} name='from_name' {...register('from_name', { required: true })}></input>
 
                 <label className={styles.label}>Email</label>
-                <input className={styles.input} {...register('email', { required: true, pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/ })}></input>
+                <input className={styles.input} name='email' {...register('email', { required: true, pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/ })}></input>
 
                 <label className={styles.label}>Message</label>
-                <textarea className={styles.input} {...register('message', { required: true })} />
+                <textarea className={styles.input} name='message' {...register('message', { required: true })} />
 
                 <div className={styles.buttons}>
                     <button className={styles.buttonSend} type='submit'>Send</button>
