f66d2f7ea518a8178da25f3a752c620a9b2db5c9
Russel Benito <59615148+9russelb@users.noreply.github.com>
2023-05-13 19:54:41 -0600

    Fleshed out About page a little bit

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,7 +13,7 @@ export default function About() {
                 <p className={styles.pronouns}>he/him</p>
             </div>
 
-            <div className={styles.schoolBackground}>
+            <div className={styles.devBackground}>
                 <p>
                     I graduated from the Northern Alberta Institute of Technology (NAIT)'s Digital Media and IT - Computer Software Development program in September 2022.
                 </p>
@@ -21,6 +21,30 @@ export default function About() {
                     During my time there, I learned a multitude of languages, skills, and a foundation of knowledge surrounding the software development life cycle
                     — from programming languages such as C#, SQL, and JavaScript to project management, communications, and agile/scrum methodologies.
                 </p>
+            </div>
+
+            <div className={styles.interests}>
+                <p>Outside of software development, I enjoy music, going to the gym, and video games.</p>
+                <div>
+                    <p>
+                        Music has always been an important part of my life. I took up the guitar at the age of 10, and continue to play to this day (albeit terribly).
+                        The storytelling and lyricism in hip-hop specifically always captivated me.
+                        Music has also taken a more physical form in my life, as I have recently started a vinyl record collection.
+                        I currently have 14 records, 6 of which are hung up on one of my bedroom walls, while the others wait because I can't afford to buy more shelving
+                        at the moment.
+                        I believe that music and sound, or a lack thereof, can help portray expression in media.
+                        Movies like Baby Driver and Guardians of the Galaxy wouldn't be what they are without the music.
+                    </p>
+                </div>
+                <div>
+                    Around August 2021, I started going to the gym. Initially, it was just another thing to do with my friends while attempting to improve my appearance.
+                    Now, it has become a routine that has improved my physical and mental health.
+                </div>
+                <div>
+                    Video games have always been prevalent in my life. Growing up, my cousins and I would play my grandfather's games almost daily. I vividly recall
+                    playing GoldenEye 007 on the N64 after eating dinner. Eventually, I would get a computer at home, and I could play games whenever I wanted to,
+                    leading to the destruction of my eyesight.
+                </div>
             </div>
         </>
     )
