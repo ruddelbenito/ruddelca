900a6304dc4f0d4f479e0835c16b931ec9f18c0b
Russel Benito <59615148+9russelb@users.noreply.github.com>
2023-05-22 15:04:25 -0600

    Fleshed out development portion, added styling to interests

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,13 +14,21 @@ export default function About() {
             </div>
 
             <div className={styles.devBackground}>
-                <p>
-                    I graduated from the Northern Alberta Institute of Technology (NAIT)'s Digital Media and IT - Computer Software Development program in September 2022.
-                </p>
-                <p>
-                    During my time there, I learned a multitude of languages, skills, and a foundation of knowledge surrounding the software development life cycle
-                    — from programming languages such as C#, SQL, and JavaScript to project management, communications, and agile/scrum methodologies.
-                </p>
+                <div className={styles.school}>
+                    <p>
+                        I graduated from the Northern Alberta Institute of Technology (NAIT)'s Digital Media and IT - Computer Software Development program in September 2022.
+                    </p>
+                    <p>
+                        During my time there, I learned a multitude of languages, skills, and a foundation of knowledge surrounding the software development life cycle
+                        — from programming languages such as C#, SQL, and JavaScript to project management, communications, and agile/scrum methodologies.
+                    </p>
+                </div>
+                <div className={styles.postSchool}>
+                    <p>
+                        After school, I decided to work part-time while in pursuit of a software career. I took on a project that I would work on during my off-days, which
+                        gave me more software experience and a sense of direction career-wise.
+                    </p>
+                </div>
             </div>
 
             <div className={styles.interests}>
@@ -28,16 +36,13 @@ export default function About() {
                 <div className={styles.music}>
                     <p>
                         Music has always been an important part of my life. I took up the guitar at the age of 10, and continue to play to this day (albeit terribly).
-                        The storytelling and lyricism in hip-hop specifically always captivated me.
+                        I believe that music and sound, or a lack thereof, can help portray expression in media.
+                        Movies like Baby Driver and Guardians of the Galaxy wouldn't be what they are without the music.
                     </p>
                     <p>
                         Music has also taken a more physical form in my life, as I have recently started a vinyl record collection.
                         I currently have 14 records, 6 of which are hung up on one of my bedroom walls, while the others wait because I can't afford to buy more shelving
                         at the moment.
-                    </p>
-                    <p>
-                        I believe that music and sound, or a lack thereof, can help portray expression in media.
-                        Movies like Baby Driver and Guardians of the Galaxy wouldn't be what they are without the music.
                     </p>
                 </div>
                 <div className={styles.gym}>
