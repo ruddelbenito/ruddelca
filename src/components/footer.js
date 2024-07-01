8b5b8dee09176052a6a0fe1d8e12b4bb38757053
Russel Benito <ruddel.benito@gmail.com>
2024-07-01 10:05:23 -0600

    Automate footer year

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,15 @@
+import moment from 'moment'
 import InstagramIcon from '../../public/assets/svgs/instagram.svg'
 import GithubIcon from '../../public/assets/svgs/github.svg'
 import LinkedInIcon from '../../public/assets/svgs/linkedin.svg'
 
 export default function Footer() {
+    const year = moment().format("YYYY");
+
     return (
         <div>
             <div>
-                <p>ruddel 2023</p>
+                <p>ruddel · {year}</p>
                 <div>
                     <a href='https://www.instagram.com/ruddel.ca/' target='_blank' rel='noreferrer'><InstagramIcon /></a>
                     <a href='https://github.com/ruddelbenito' target='_blank' rel='noreferrer'><GithubIcon /></a>
