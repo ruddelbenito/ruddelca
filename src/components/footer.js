c275504c1529e7319bcfe4d0d34f61fd979dff66
Russel Benito <ruddel.benito@gmail.com>
2024-07-01 10:25:34 -0600

    Start footer styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,19 +2,18 @@ import moment from 'moment'
 import InstagramIcon from '../../public/assets/svgs/instagram.svg'
 import GithubIcon from '../../public/assets/svgs/github.svg'
 import LinkedInIcon from '../../public/assets/svgs/linkedin.svg'
+import styles from '@/styles/footer.module.scss'
 
 export default function Footer() {
     const year = moment().format("YYYY");
 
     return (
-        <div>
-            <div>
-                <p>ruddel · {year}</p>
-                <div>
-                    <a href='https://www.instagram.com/ruddel.ca/' target='_blank' rel='noreferrer'><InstagramIcon /></a>
-                    <a href='https://github.com/ruddelbenito' target='_blank' rel='noreferrer'><GithubIcon /></a>
-                    <a href='https://www.linkedin.com/in/russel-benito-284261226/' target='_blank' rel='noreferrer'><LinkedInIcon /></a>
-                </div>
+        <div className={styles.footer}>
+            <p className={styles.branding}>ruddel · {year}</p>
+            <div className={styles.socials}>
+                <a href='https://www.instagram.com/ruddel.ca/' target='_blank' rel='noreferrer'><InstagramIcon /></a>
+                <a href='https://github.com/ruddelbenito' target='_blank' rel='noreferrer'><GithubIcon /></a>
+                <a href='https://www.linkedin.com/in/russel-benito-284261226/' target='_blank' rel='noreferrer'><LinkedInIcon /></a>
             </div>
         </div>
     )
